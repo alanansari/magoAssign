719e56086334bfe14000de96f573e3d53e8b7021
Alan <ansarialan31@gmail.com>
2023-04-26 06:02:43 +0530

    ADD: rate limited API's

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {auth} = require('../middleware/authverify');
 const urlController = require('../controller/urlController');
+const rateLimit = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000,
+	max: 10,
+	standardHeaders: true,
+	legacyHeaders: false
+})
 
 router.get('/:urlId',urlController.see);
-router.post('/short',auth,urlController.short);
+router.post('/short',limiter,auth,urlController.short);
 
 module.exports = router;
